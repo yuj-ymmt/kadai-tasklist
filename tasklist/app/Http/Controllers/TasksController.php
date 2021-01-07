469d7302fdfde8bd50ce07ab62177142ec25dfd4
yuj-ymmt <yoji_yamamoto@virtual-entertainment.biz>
2021-01-07 06:45:36 +0000

    add column

--- tasklist/app/Http/Controllers/TasksController.php
+++ tasklist/app/Http/Controllers/TasksController.php
@@ -43,8 +43,12 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+                "status" => "required|Max:10"
+            ]);
         //
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -93,7 +97,12 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $request->validate([
+                "status" => "required|Max:10"
+            ]);
+            
         $task = Task::findOrFail($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
--- tasklist/resources/views/layouts/app.blade.php
+++ tasklist/resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
         @include('commons.navbar')
 
         <div class="container">
+            @include("commons.error_messages")
             @yield('content')
         </div>
 
